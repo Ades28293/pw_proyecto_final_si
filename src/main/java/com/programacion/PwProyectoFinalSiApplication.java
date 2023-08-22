692aa98990dd3f558a10b941740207c850b7fc51
ades28293 <cenyungamichael@gmail.com>
2023-08-22 13:23:10 -0500

    first commit

---
+++ src/main/java/com/programacion/PwProyectoFinalSiApplication.java
@@ -0,0 +1,13 @@
+package com.programacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwProyectoFinalSiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwProyectoFinalSiApplication.class, args);
+	}
+
+}
